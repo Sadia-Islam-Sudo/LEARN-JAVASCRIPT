06f40c5263ee274f3364a9523037898b90202be0
Sadia-Islam-Sudo <sadiaredia.2023@gmail.com>
2026-03-11 21:59:48 +0600

    comment,error showing

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 console.log("Script js") //execute in backend named node.js . Open terminal then type node .\script.js Then can watch the written things which is shown in script.js file .
+
+//single line comment 
+/* multi line comment
+hello */
+
+console.log("error")
+console.log("error")
+console.warn("warning")
+
+console.log("hello", "world, 3")
